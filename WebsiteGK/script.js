5c6c6d06caa8f68bd89586207fc58ac96db19c30
Kiavash Seraj <serajkx@dukes.jmu.edu>
2022-12-28 19:38:06 -0500

    Added side bar and finished tab layout

--- WebsiteGK/script.js
+++ WebsiteGK/script.js
@@ -20,7 +20,7 @@ let loginData;
             console.log(element);
             if (username == element.Username) {
                 if (password == element.Password) {
-                    window.location = "https://www.google.com";
+                    window.location = "http://127.0.0.1:5500/GuildKeeper/WebsiteGK/BSHome.html";
                 } else {
                     console.log("failure");
                 }
